e3265034cdde8dce0feb01ffdebc496e2c9f0f15
kasia.kordys <kasia.kordys@gmail.com>
2020-12-02 20:15:32 +0000

    add model

---
+++ MvcModels/Models/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MvcModels.Models
+{
+    public class Person
+    {
+        //[DisplayName("New Person")]
+        //[HiddenInput]
+        //[HiddenInput(DisplayValue = false
+        //[Display(Name = "Nickname")]
+        //[DataType(DataType.Date)]
+        public int PersonId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birthday { get; set; }
+        public Address HomeAddress { get; set; }
+        public bool isApproved { get; set; }
+        public Role Role { get; set; }
+
+    }
+
+    public class Address
+    {
+        public string Line1 { get; set; }
+        public string Line2 { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+    }
+
+    public enum Role
+    {
+        Admin,
+        User,
+        Guest
+    }
+}
