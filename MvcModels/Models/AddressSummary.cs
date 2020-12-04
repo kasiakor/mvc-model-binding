e8cc55082b3428a51b8db91127aa393ac2955afc
kasia.kordys <kasia.kordys@gmail.com>
2020-12-04 19:06:09 +0000

    apply bind attribute to model

--- MvcModels/Models/AddressSummary.cs
+++ MvcModels/Models/AddressSummary.cs
@@ -1,10 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web.Mvc;
 
 namespace MvcModels.Models
 {
+
+    //only citywill be included in binding process
+    [Bind(Include = "City")]
     public class AddressSummary
     {
         public string City { get; set; }
